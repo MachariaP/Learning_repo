deccf6c4cc0f8d952960f853d5f0833393476e70
MachariaP <walburphinehas78@gmail.com>
2024-10-13 12:57:47 +0300

    created a view to handle the cv input form

--- djangoApps/Resume_generator/cv_generator/cv_builder/views.py
+++ djangoApps/Resume_generator/cv_generator/cv_builder/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profile
 
 # Create your views here.
+def create__profile(request):
+    if request.method == 'POST':
+        Profile = Profile(
+            name=request.POST['name'],
+            phone_number=request.POST['phone_number'],
+            email=request.POST['email'],
+            degree=request.POST['degree'],
+            skills=request.POST['skills'],
+            about=request.POST['about']
+        )
