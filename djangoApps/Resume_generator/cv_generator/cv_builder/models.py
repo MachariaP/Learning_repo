25761310bc45ab974ee7faefdc1759c78d4f689e
MachariaP <walburphinehas78@gmail.com>
2024-10-13 12:19:49 +0300

    Registered the Model in admin

--- djangoApps/Resume_generator/cv_generator/cv_builder/models.py
+++ djangoApps/Resume_generator/cv_generator/cv_builder/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
+    name: models.CharField(max_length=100)
+    phone_number: models.CharField(max_length=15)
+    email: models.EmailField()
+    degree: models.CharField(max_length=100)
+    skills: models.TextField()
+    about: models.TextField()
+
+    def __str__(self):
+        return self.name
+    
