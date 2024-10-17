272b5b8ca55dc8b97a1545a92c8458429f55e596
MachariaP <walburphinehas78@gmail.com>
2024-10-17 11:29:02 +0300

    Update URL and views for CV generator

--- djangoApps/Resume_generator/cv_generator/cv_builder/urls.py
+++ djangoApps/Resume_generator/cv_generator/cv_builder/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('create/', views.create_profile, name='create_profile'),
-    path('success/', views.success, name='success'),
+    path('success/<int:profile_id>/', views.success, name='success'),
+    path('generate_pdf/<int:profile_id>/', views.generate_pdf, name='generate_pdf'),
 ]
--- djangoApps/Resume_generator/cv_generator/cv_builder/views.py
+++ djangoApps/Resume_generator/cv_generator/cv_builder/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from .models import Profile
+import pdfkit
 
-# Create your views here.
-def create__profile(request):
+def create_profile(request):
     if request.method == 'POST':
-        Profile = Profile(
+        profile = Profile(
             name=request.POST['name'],
             phone_number=request.POST['phone_number'],
             email=request.POST['email'],
@@ -12,3 +14,17 @@ def create__profile(request):
             skills=request.POST['skills'],
             about=request.POST['about']
         )
+        profile.save()
+        return redirect('success', profile_id=profile.id)
+    return render(request, 'cv_builder/create_profile.html')
+
+def success(request, profile_id):
+    return render(request, 'cv_builder/success.html', {'profile_id': profile_id})
+
+def generate_pdf(request, profile_id):
+    profile = Profile.objects.get(pk=profile_id)
+    html = render_to_string('cv_builder/resume.html', {'profile': profile})
+    pdf = pdfkit.from_string(html, False)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="resume.pdf"'
+    return response
