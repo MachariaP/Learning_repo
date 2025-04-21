5592428ff989353a1d5d33faab87ec352360a20e
Phinehas <walburphinehas78@gmail.com>
2025-04-21 12:44:48 +0300

    feat: define Car class and create instances with speed and color attributes

---
+++ Python_tutorials/alxstudy/ooprogramming/objectOrientedProgramming/Car.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+class Car:
+    pass
+
+toyota = Car()
+honda = Car()
+audi = Car()
+
+toyota.speed = 80
+honda.speed = 90
+audi.speed = 120
+
+toyota.color = "Red"
+honda.color = "Black"
+audi.color = "Blue"
+
+print(toyota.speed)
+print(honda.speed)
+print(audi.speed)
+print(toyota.color)
+print(audi.color)
+
