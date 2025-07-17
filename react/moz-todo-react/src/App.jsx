e46f748c7f2b38e7c98036badecb9ac2a66a7313
Phinehas <walburphinehas78@gmail.com>
2025-07-17 15:09:00 +0300

    Update React app: modified App.jsx, main.jsx, and App.css for initial component setup

--- react/moz-todo-react/src/App.jsx
+++ react/moz-todo-react/src/App.jsx
@@ -1,11 +1,16 @@
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
-function App() {
+function App(props) {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <header>
-        <h1>Hello, World!</h1>
-        <button type="button">Click me!</button>
+        <h1>Hello, {props.subject}!</h1>
+        <button type="button" className="primary" onClick={() => setCount(count + 1)}>
+          Clicked {count} times
+        </button>
       </header>
     </>
   );
