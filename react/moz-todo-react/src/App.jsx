13e091ea384b365a958efc40276d11827a958588
Phinehas <walburphinehas78@gmail.com>
2025-07-18 09:23:44 +0300

    Initialized App component with basic structure and TodoMatic title
    Created the initial version of src/App.jsx as a React functional component. Defined
    the App function that accepts props and returns a basic JSX structure with a div
    container and an h1 element displaying /TodoMatic/. Included the export default
    statement to make the component importable.

--- react/moz-todo-react/src/App.jsx
+++ react/moz-todo-react/src/App.jsx
@@ -1,18 +1,8 @@
-import { useState } from 'react';
-import './App.css';
-
 function App(props) {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <header>
-        <h1>Hello, {props.subject}!</h1>
-        <button type="button" className="primary" onClick={() => setCount(count + 1)}>
-          Clicked {count} times
-        </button>
-      </header>
-    </>
+    <div>
+      <h1>TodoMatic</h1>
+    </div>
   );
 }
 
